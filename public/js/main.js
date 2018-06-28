22a17cb1d077e11e5272dc68c3aaebcd7f46849c
Philippe Luchansky <philuchansky@gmail.com>
2018-06-27 19:28:27 -0700

    render graphs

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+const $form = $('form')
+const $textInput = $('#text')
+const apiClient = axios.create()
+const $formSubmit = $('#form-submit')
+const $formModal = $('#form-modal')
+
+// progress bars
+const $joy = $('#joy')
+const $disgust = $('#disgust')
+const $sadness = $('#sadness')
+const $fear = $('#fear')
+const $anger = $('#anger')
+
+$form.on('submit', (evt) => {
+  evt.preventDefault()
+  $formSubmit.attr('disabled', true)
+  const text = $textInput.val()
+  apiClient({ method: 'get', url: `/analyze?text=${text}` }).then((apiResponse) => {
+    const emotionTone = apiResponse.data.document_tone.tone_categories["0"].tones
+    console.log(emotionTone)
+    $formSubmit.attr('disabled', false)
+    $formModal.modal('hide')
+
+    $joy.css({ width: (emotionTone[3].score * 100) + '%' })
+    $disgust.css({ width: (emotionTone[1].score * 100) + '%' })
+    $sadness.css({ width: (emotionTone[4].score * 100) + '%' })
+    $fear.css({ width: (emotionTone[2].score * 100) + '%' })
+    $anger.css({ width: (emotionTone[0].score * 100) + '%' })
+  })
+})
--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const
   express = require('express'),
   app = express(),
   logger = require('morgan'),
-  ejsLayouts = require('express-ejs-layouts')
-  PORT = 3000
+  ejsLayouts = require('express-ejs-layouts'),
+  axios = require('axios'),
+  PORT = 3000,
+  ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3')
+
+const  { WATSON_URL, WATSON_USERNAME, WATSON_PASSWORD} = process.env
+const toneAnalyzer = new ToneAnalyzerV3({
+  username: WATSON_USERNAME,
+  password: WATSON_PASSWORD,
+  version: '2016-05-19',
+  url: WATSON_URL
+})
 
 app.set('view engine', 'ejs')
 
@@ -13,6 +23,16 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/analyze', (req, res) => {
+  toneAnalyzer.tone({
+    tone_input: req.query.text,
+    content_type: 'text/plain'
+  }, (err, tone) => {
+    if(err) return console.log(err)
+    res.json(tone)
+  })
 })
 
 app.listen(PORT, (err) => {
